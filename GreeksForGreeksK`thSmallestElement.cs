ae28f812dc26dfca2dc45f0410815de93141a428
Diganta208 <57087564+Diganta208@users.noreply.github.com>
2020-07-10 05:49:28 -0700

    Add files via upload

---
+++ GreeksForGreeksK`thSmallestElement.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Given an array arr[] and a number K where K is smaller than size of array, the task is to find the Kth 
+             * smallest element in the given array. It is given that all array elements are distinct.*/
+
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for(int i =0; i<arr.Count(); i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+           
+
+            for(int i=0; i<arr.Count()-1; i++)
+            {
+                 int min = i;
+                for(int j=i+1; j<arr.Count(); j++)
+                {
+                    if(arr[j]<arr[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                if(min!=i)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[min];
+                    arr[min] = temp;
+                }
+            }
+
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(arr[k-1]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GreeksForGreeksKadane'sAlgorithm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Given an array arr of N integers. Find the contiguous sub-array with maximum sum.*/
+
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for(int i=0; i< arr.Count(); i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int maxSum = -100;
+
+            int sum = 0;
+
+
+            for(int i=0; i<arr.Count()-1; i++)
+            {
+                sum = arr[i];
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                }
+                for(int j=i+1; j<arr.Count(); j++)
+                {
+                    sum = sum + arr[j];
+                    if(sum>maxSum)
+                    {
+                        maxSum = sum;
+                    }
+
+                }
+
+                sum = 0;
+
+            }
+
+            Console.WriteLine(maxSum);
+            Console.ReadKey();
+        }
+    }
+}
