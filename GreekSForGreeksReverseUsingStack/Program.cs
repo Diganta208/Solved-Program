79a790c7d3eceeec6f590891f16dffff18922e49
Diganta208 <57087564+Diganta208@users.noreply.github.com>
2020-07-10 23:23:22 -0700

    Add files via upload

---
+++ GreekSForGreeksReverseUsingStack/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*An string of words is given, the task is to reverse the string using stack.*/
+
+
+
+            string aString = Console.ReadLine();
+
+            Char[] arr = aString.ToCharArray();
+
+            Stack aStack = new Stack(arr.Count());
+
+            for(int i=0; i<arr.Count(); i++)
+            {
+                aStack.push(arr[i]);
+            }
+
+            for (int i = 0; i < arr.Count(); i++)
+            {
+               char result= aStack.pop();
+               Console.Write(result);
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ GreekSForGreeksReverseUsingStack/Stack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+    class Stack
+    {
+        char[] charArr;
+        int top;
+        public Stack( int no)
+        {
+            charArr = new char[no];
+             top = 0;
+
+        }
+
+        public void push(char Data)
+        {
+            if(top>=charArr.Count())
+            {
+                Console.WriteLine("Array is Full");
+            }
+            else
+            {
+                this.charArr[top] = Data;
+                top++;
+            }
+        }
+
+
+        public char pop()
+        {
+            char item;
+            if (top == 0)
+            {
+                Console.WriteLine("There is no element in this stack");
+                return '-';
+            }
+            else
+            {
+                top--;
+                item = charArr[top];
+                return item;
+
+            }
+        }
+    }
+}
