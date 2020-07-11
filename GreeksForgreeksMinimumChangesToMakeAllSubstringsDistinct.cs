c74cba4ee7cfd7fecedc423a14cd483a714d514e
Diganta208 <57087564+Diganta208@users.noreply.github.com>
2020-07-11 07:39:27 -0700

    Add files via upload

---
+++ GreeksForgreeksMinimumChangesToMakeAllSubstringsDistinct.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Given a string s, find the minimum number of changes required to it so that all substrings of the string become distinct.*/
+
+
+
+            string input = Console.ReadLine();
+            char[] CharArr = input.ToCharArray();
+
+            List<char> Result = new List<char>();
+
+            int count = 0;
+
+            for(int i=0; i<CharArr.Count(); i++)
+            {
+                if(Result.Contains(CharArr[i]))
+                {
+                    count++;
+                }
+                else
+                {
+                    Result.Add(CharArr[i]);
+                }
+            }
+
+            Console.WriteLine(count);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ greeksForGreeksSmallestPowerOf2GreaterThanOrEqualton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Given a number N, Write a program to find a number which is greater than or equal to N and is a smallest power of 2.*/
+
+
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=0; i<n; i++)
+            {
+                int po = Convert.ToInt32(Math.Pow(2,i));
+
+                if(po>=n)
+                {
+                    Console.WriteLine(po);
+                    break;
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
