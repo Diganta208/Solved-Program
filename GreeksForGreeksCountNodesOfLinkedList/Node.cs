ea1f90ed0ae2ee18db8f4fb93f3f80e39ca58aa8
Diganta208 <57087564+Diganta208@users.noreply.github.com>
2020-07-24 08:57:39 -0700

    Add files via upload

---
+++ GreeksForGreeksCountNodesOfLinkedList/Node.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication26
+{
+    class Node
+    {
+
+        private int data { get; set; }
+        private Node next { get; set; }
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+
+
+
+        public void addToTial(int data, Node head)
+        {
+            if (head == null)
+            {
+                Node newHead = new Node(data);
+
+            }
+            else
+            {
+                Node currentNode = head;
+                Node newNode = new Node(data);
+
+                while (currentNode.next != null)
+                {
+                    currentNode = currentNode.next;
+                }
+
+
+                currentNode.next = newNode;
+              }
+
+
+        }
+
+        public void CountNode( Node head)
+        {
+            Node CurrentNode = head;
+            int count = 1;
+
+
+            while(CurrentNode.next!=null)
+            {
+                CurrentNode = CurrentNode.next;
+                count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ GreeksForGreeksCountNodesOfLinkedList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Given a singly linked list. The task is to find the length of the linked list, where length is defined as the number of nodes in the linked list.*/
+
+
+            Node aNode = new Node(5);
+            aNode.addToTial( 10, aNode);
+            aNode.addToTial(15, aNode);
+            aNode.addToTial(20, aNode);
+            aNode.addToTial(25, aNode);
+            aNode.addToTial(30, aNode);
+            aNode.addToTial(35, aNode);
+
+            aNode.CountNode(aNode);
+            Console.ReadKey();
+        }
+    }
+}
