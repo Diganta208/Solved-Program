0069ef67c53bb8114dbcab5e6411004f175a646a
Diganta208 <57087564+Diganta208@users.noreply.github.com>
2020-07-24 09:29:22 -0700

    Add files via upload

---
+++ GreeksForGreeksCheckIfLinkedListIsPalindrome/Node.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication27
+{
+    class Node
+    {
+
+        private int data { get; set; }
+        private Node next { get; set; }
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+
+
+
+        public void addToTial(int data, Node head)
+        {
+            if (head == null)
+            {
+                Node newHead = new Node(data);
+
+            }
+            else
+            {
+                Node currentNode = head;
+                Node newNode = new Node(data);
+
+                while (currentNode.next != null)
+                {
+                    currentNode = currentNode.next;
+                }
+
+
+                currentNode.next = newNode;
+            }
+
+
+        }
+
+        private int CountNode(Node head)
+        {
+            Node CurrentNode = head;
+            int count = 1;
+
+
+            while (CurrentNode.next != null)
+            {
+                CurrentNode = CurrentNode.next;
+                count++;
+            }
+
+            return count;
+        }
+
+        public void  isPalindrome(Node head)
+        {
+             int no=this.CountNode(head);
+             int[] arr = new int[no];
+
+             Node CurrentNode = head;
+
+             int i = 0;
+             while (CurrentNode.next != null)
+             {
+                 arr[i] = CurrentNode.data;
+                 CurrentNode = CurrentNode.next;
+                 i++;
+             }
+
+             arr[i] = CurrentNode.data;
+
+            string str=null;
+
+            for(int j=0; j<arr.Count(); j++)
+            {
+                str = str + arr[j].ToString();
+            }
+
+            string revStr = null;
+
+            for (int j = arr.Count()-1; j >=0; j--)
+            {
+                revStr = revStr + arr[j].ToString();
+            }
+
+            if(str== revStr)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine(" Not Palindrome");
+            }
+        }
+
+    }
+}
---
+++ GreeksForGreeksCheckIfLinkedListIsPalindrome/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Given a singly linked list of size N of integers. The task is to check if the given linked list is palindrome or not.*/
+
+
+
+            Node aNode = new Node(1);
+            aNode.addToTial(2, aNode);
+            aNode.addToTial(1, aNode);
+
+            aNode.isPalindrome(aNode);
+            Console.ReadKey();
+        }
+    }
+}
