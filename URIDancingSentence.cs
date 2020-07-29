2ab44df24bf2b99d70b9b31bc877a6ee7ffc636d
Diganta208 <57087564+Diganta208@users.noreply.github.com>
2020-07-28 22:40:10 -0700

    Add files via upload

---
+++ URIDancingSentence.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] inputStr = input.ToCharArray();
+            string previousCarecter = null;
+            string result = null;
+
+            for(int i=0; i<inputStr.Count(); i++)
+            {
+                if(Char.IsLetter(inputStr[i]))
+                {
+                    if(previousCarecter==null)
+                    {
+                        
+                           string s= inputStr[i].ToString();
+                          string newS= s.ToUpper();
+                          result = result + newS;
+                           previousCarecter = "upper";
+                     
+                    }
+                    else if(previousCarecter=="upper")
+                    {
+                        string s = inputStr[i].ToString();
+                        string newS = s.ToLower();
+                        result = result + newS;
+                        previousCarecter = "Lower";
+                    }
+                    else
+                    {
+                        string s = inputStr[i].ToString();
+                        string newS= s.ToUpper();
+                        result = result + newS;
+                        previousCarecter = "upper";
+                    }
+
+                   
+                }
+                else
+                {
+                    result = result + inputStr[i].ToString();
+                }
+            }
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
