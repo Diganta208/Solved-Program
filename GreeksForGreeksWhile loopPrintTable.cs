e721a3cea383d6aed63922cb369390a96fc9f405
Diganta208 <57087564+Diganta208@users.noreply.github.com>
2020-07-30 21:59:06 -0700

    Add files via upload

---
+++ GreeksForGreeksWhile loopPrintTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int no = Convert.ToInt32(Console.ReadLine());
+
+            int i=10;
+             while(i>0)
+             {
+                 Console.Write(no * i +" ");
+                 i--;
+
+             }
+             Console.ReadKey();
+        }
+    }
+}
