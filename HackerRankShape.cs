f575727001c933032f76f2d713b308a721f2730d
Diganta208 <57087564+Diganta208@users.noreply.github.com>
2020-07-30 22:27:28 -0700

    Add files via upload

---
+++ HackerRankShape.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int no = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int j = 0; j < no; j++)
+            {
+                for (int i = 0; i < no; i++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write("*" + " ");
+                    }
+                    else if (i == no - j - 1)
+                    {
+                        Console.Write("*" + " ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
