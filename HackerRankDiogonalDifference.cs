6568a81c8dfaf05a770577686c79dc40f5e35edd
Diganta208 <57087564+Diganta208@users.noreply.github.com>
2020-08-05 12:25:01 +0600

    Add files via upload

---
+++ HackerRankDiogonalDifference.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int no = Convert.ToInt32(Console.ReadLine());
+            int[,] input = new int[no, no];
+
+            for (int i = 0; i < no; i++)
+            {
+                for (int j = 0; j < no; j++)
+                {
+                    input[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            int firstDiogonal = 0;
+            int secondDiogonal = 0;
+
+            for (int i = 0; i < no; i++)
+            {
+                for (int j = 0; j < no; j++)
+                {
+                   if(i==j)
+                   {
+                       firstDiogonal = firstDiogonal + input[i,j];
+                   }
+
+                    if( j==no-i-1)
+                    {
+                        secondDiogonal = secondDiogonal + input[i, j];
+                    }
+                }
+            }
+
+            if(firstDiogonal>=secondDiogonal)
+            {
+                Console.WriteLine(firstDiogonal - secondDiogonal);
+            }
+            else
+            {
+                Console.WriteLine(secondDiogonal - firstDiogonal);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
