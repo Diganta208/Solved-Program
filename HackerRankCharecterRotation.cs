e2ff8be4207fbd3f520b4f96f18dcb0fd467bdfd
Diganta dey <57087564+Diganta208@users.noreply.github.com>
2020-09-19 21:19:34 +0600

    Add files via upload

---
+++ HackerRankCharecterRotation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Input a String :");
+            string input = Console.ReadLine();
+
+
+
+            char[] inputStr = input.ToCharArray();
+
+            string result = null;
+
+            for(int i=0; i<inputStr.Count(); i++)
+            {
+                
+                if(inputStr[i]=='x'|| inputStr[i]=='y'|| inputStr[i]=='z'||inputStr[i]=='X'|| inputStr[i]=='Y'|| inputStr[i]=='Z')
+                {
+                    int value = Convert.ToInt32(inputStr[i]) - 23;
+                    char temp = Convert.ToChar(value);
+                    result = result + temp.ToString();
+
+                }
+                else
+                {
+                    int value = Convert.ToInt32(inputStr[i]) + 3;
+
+                    char temp = Convert.ToChar(value);
+                    result = result + temp.ToString();
+                }
+               
+            }
+            Console.WriteLine("Rotate String :");
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
