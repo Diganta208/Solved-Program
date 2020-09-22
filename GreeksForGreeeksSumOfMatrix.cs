946fe84bb566602748d0e1005461e79e9b2cf132
Diganta dey <57087564+Diganta208@users.noreply.github.com>
2020-09-22 21:26:16 +0600

    Add files via upload

---
+++ GreeksForGreeeksSumOfMatrix.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Row No");
+            int i= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Coloumn No");
+            int j= Convert.ToInt32(Console.ReadLine());
+
+            int[,] arr = new int[i, j];
+            
+            Console.WriteLine("Please Enter Value of 2d array");
+
+            for (int m = 0; m < i; m++)
+            {
+                for (int n = 0; n < j; n++)
+                {
+                    arr[m, n] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+
+            for(int m=0; m<i; m++)
+            {
+                for(int n=0; n<j; n++)
+                {
+                    Console.Write(arr[m, n] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+       
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ GreesForGreeks2dArrayReprasentation.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Size of Array");
+
+            int i = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[i];
+            
+            Console.WriteLine("Please Enter Value array");
+
+            for (int m = 0; m < i; m++)
+            {
+               
+                    arr[m] = Convert.ToInt32(Console.ReadLine());
+                
+            }
+
+          
+
+            int rightHandSide = 0;
+            int leftHandSide = 0;
+            string status= "Not found";
+
+
+            if(i==1)
+            {
+                Console.WriteLine("Equlubriam is:" + arr[i-1]);
+            }
+            else if(i==2)
+            {
+                Console.WriteLine("Equlubriam not present");
+            }
+            else
+            {
+                for (int m = 0; m < i; m++)
+                {
+                    int check = arr[m];
+                    for(int p=0; p<m; p++)
+                    {
+                        leftHandSide = leftHandSide + arr[p];
+                    }
+                    for(int q=m+1; q<i; q++)
+                    {
+                        rightHandSide = rightHandSide + arr[q];
+                    }
+                    if(leftHandSide==rightHandSide)
+                    {
+                        Console.WriteLine("Equlubriam is : " + check);
+                        status = "Found";
+                        break;
+                    }
+                    else
+                    {
+                        rightHandSide = 0;
+                        leftHandSide = 0;
+                    }
+                }
+
+                if(status == "Not found")
+                {
+                    Console.WriteLine("Equlubriam not present");
+                }
+            }
+
+            
+
+       
+            Console.ReadKey();
+
+        }
+    }
+}
